64cf62d6716b82bcfe0155a8862ae243f844dd00
duzhaolin <dzlkuboy@163.com>
2022-10-18 17:10:46 +0800

    123

--- open-in-editor.js
+++ open-in-editor.js
@@ -1,16 +1,15 @@
-const launch = require('launch-editor');
-
+const launch = require("launch-editor");
 
 launch(
   // filename:line:column
   // both line and column are optional
-  './docs/javascript/vue相关笔记/index.md:6:52',
+  "./docs/javascript/vue相关笔记/index.md:6:52",
   // try specific editor bin first (optional)
-  'code',
+  "code",
   // callback if failed to launch (optional)
   (fileName, errorMsg) => {
-    console.log('fileName', fileName)
-    console.log('errorMsg', errorMsg)
+    console.log("fileName", fileName);
+    console.log("errorMsg", errorMsg);
     // log error if any
   }
-)
+);
